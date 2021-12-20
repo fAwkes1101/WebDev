068c1b8cc02a8f8ced4c6ba5d808eecf4114c6c8
Abhishek Yadav <abhishekyadav0751@gmail.com>
2021-12-20 23:58:45 +0530

    vcr

--- Javascript/20_dec/Anonymous.js
+++ Javascript/20_dec/Anonymous.js
@@ -1,7 +1,7 @@
-//Anonymous function
+//Anonymous function -> jinke namm na ho
 
 //1.Funcion expression.
-let funcVar = function sayHi(){
+let funcVar = function (){
     console.log("hello");
 }
 funcVar(); //function ko ek variable me store kr liya then uss variable ko use karke func ko call lg liya
